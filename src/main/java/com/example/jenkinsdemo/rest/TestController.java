b87198591d8e8c91c53cb44ba4cce91a1a153656
Young <446538529@qq.com>
2020-06-07 22:24:22 +0800

    test

--- src/main/java/com/example/jenkinsdemo/rest/TestController.java
+++ src/main/java/com/example/jenkinsdemo/rest/TestController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.Random;
 
 @RestController
 public class TestController {
@@ -11,6 +12,12 @@ public class TestController {
     @GetMapping(value = "/test")
     public String test(){
         return "ok:"+new Date();
+    }
+
+    @GetMapping(value = "/ramdom")
+    public String ramdom(){
+        Random r=new Random();
+        return r.nextBoolean()+":"+new Date();
     }
 
 }
